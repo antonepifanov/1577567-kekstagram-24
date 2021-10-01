9e19c1a00df929790d9d3bc763be971191a4c42d
antonepifanov <antonepifanov87@gmail.com>
2021-10-01 16:28:10 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -2,16 +2,14 @@
 
 const getRandomInteger = (min, max) => {
   let rand;
-  if (min < 0 || max < 0) {
-    return 'Мне не разрешают работать с отрицательными числами';
-  }
-  switch (min, max) {
+  switch (true) {
+    case min < 0 || max < 0:
+      return 'Мне не разрешают работать с отрицательными числами';
     case min > max:
       rand = max - 0.5 + Math.random() * (min - max + 1);
       return Math.round(rand);
     case min===max:
-      rand = min;
-      return rand;
+      return rand = min;
     default:
       rand = min - 0.5 + Math.random() * (max - min + 1);
       return Math.round(rand);
@@ -19,6 +17,6 @@ const getRandomInteger = (min, max) => {
 };
 getRandomInteger(3, 90);
 
-const isStringLengthCorrect = (string, length) => !(string.length > length);
-isStringLengthCorrect('При выборе за основной показатель нужно взять читабельность кода. Чем код понятнее, нагляднее, тем удобнее его рефакторить (так называется улучшение кода) и поддерживать. Тернарный оператор может как сделать код проще, так и необоснованно его усложнить. Это зависит от ситуации.', 140);
+const isLengthOfStringCorrect = (string, length) => !(string.length > length);
+isLengthOfStringCorrect('При выборе за основной показатель нужно взять читабельность кода. Чем код понятнее, нагляднее, тем удобнее его рефакторить (так называется улучшение кода) и поддерживать. Тернарный оператор может как сделать код проще, так и необоснованно его усложнить. Это зависит от ситуации.', 140);
 
