f31352111379e8a570301caab2f9a1f04afc501c
antonepifanov <antonepifanov87@gmail.com>
2021-10-15 22:35:31 +0300

    Модуль get-messages

--- js/utils/get-messages.js
+++ js/utils/get-messages.js
@@ -2,16 +2,16 @@ import {getRandomPositiveInteger} from './get-random-positive-integer.js';
 import {MIN_AMOUNT_OF_MESSAGE, MAX_AMOUNT_OF_MESSAGE} from '../constants.js';
 
 const getMessages = (arrayOfComments) => {
-  let message = '';
+  const message = [];
   const numberOfMessage = getRandomPositiveInteger(MIN_AMOUNT_OF_MESSAGE, MAX_AMOUNT_OF_MESSAGE);
   for (let i = 0; i < numberOfMessage; i++) {
     let randomMessageIndex = getRandomPositiveInteger(0, arrayOfComments.length - 1);
     while (message === arrayOfComments[randomMessageIndex]) {
       randomMessageIndex = getRandomPositiveInteger(0, arrayOfComments.length - 1);
     }
-    message += arrayOfComments[randomMessageIndex];
+    message.push(arrayOfComments[randomMessageIndex]);
   }
-  return message;
+  return message.join(' ');
 };
 
 export {getMessages};
