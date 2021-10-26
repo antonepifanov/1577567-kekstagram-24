11364cbc0aa29797cd484eba1c9f13d4fead44c0
antonepifanov <antonepifanov87@gmail.com>
2021-10-26 19:16:16 +0300

    Добавляет реализацию открытия-закрытия формы загрузки изображения

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 import './render-pictures.js';
 import './render-picture-modal.js';
+import './render-form-modal.js';
--- js/render-form-modal.js
+++ js/render-form-modal.js
@@ -1,3 +1,30 @@
 import {isEscapeKey} from './utils/is-escape-key.js';
-import {isEnterKey} from './utils/is-enter-key.js';
+
+const body = document.querySelector('body');
+const pictureUploadForm = body.querySelector('.img-upload__form');
+const uploadUserPictureInput = pictureUploadForm.querySelector('.img-upload__input');
+const pictureEditModal = pictureUploadForm.querySelector('.img-upload__overlay');
+const pictureEditFormCancel = pictureEditModal.querySelector('.img-upload__cancel');
+
+const onModalEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    pictureEditModal.classList.add('hidden');
+    body.classList.remove('modal-open');
+    pictureUploadForm.reset();
+  }
+};
+
+uploadUserPictureInput.addEventListener('change', () => {
+  pictureEditModal.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onModalEscKeydown);
+});
+
+pictureEditFormCancel.addEventListener('click', () => {
+  pictureEditModal.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onModalEscKeydown);
+});
+
 
