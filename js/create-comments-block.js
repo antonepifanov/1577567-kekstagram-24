a29c92a9404d4ca7d7deab6dbb1de2af49f31bd1
antonepifanov <antonepifanov87@gmail.com>
2021-10-30 22:08:06 +0300

    Добавляет кнопку загрузки и счётчик комментариев

--- js/create-comments-block.js
+++ js/create-comments-block.js
@@ -2,8 +2,12 @@ const bigPicture = document.querySelector('.big-picture');
 const bigPictureCommentsList = bigPicture.querySelector('.social__comments');
 const bigPictureCommentTemplate = document.querySelector('#social__comment').content.querySelector('.social__comment');
 const bigPictureCommentsListFragment = document.createDocumentFragment();
+const socialCommentCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
 
 const createCommentsBlock = (picture) => {
+  commentsLoader.classList.add('hidden');
+  socialCommentCount.classList.add('hidden');
   const comments = picture.comments;
   bigPictureCommentsList.innerHTML = '';
   comments.forEach(({avatar, name, message}) => {
@@ -14,6 +18,10 @@ const createCommentsBlock = (picture) => {
     bigPictureCommentsListFragment.appendChild(bigPictureComment);
   });
   bigPictureCommentsList.appendChild(bigPictureCommentsListFragment);
+  if (picture.comments.length > 5) {
+    commentsLoader.classList.remove('hidden');
+    socialCommentCount.classList.remove('hidden');
+  }
 };
 
 export {createCommentsBlock};
